ee48bcf2b25a39551e50bbf012169d9da26cf0f1
Peter Laplain <peter@laplain.co.uk>
2026-05-14 10:42:17 +0100

    Added Bad-PowerShell to test ci-powershell.yml action fails.

---
+++ src/Bad-PowerShell.ps1
@@ -0,0 +1,9 @@
+function Bad-PowerShell {
+    Write-Host "This is a bad PowerShell script!"
+    Write-Host "It does nothing useful and is just for demonstration purposes."
+    Write-Host "Don't run this script on your system!"
+
+    if ($true){
+        Write-Host "Should fail PSAnalyser"
+    }
+}
