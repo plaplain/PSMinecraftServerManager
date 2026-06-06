ed0f929f99c39428f8f8123528d9e5e84f8a8869
Peter Laplain <peter@laplain.co.uk>
2026-06-06 13:05:30 +0100

    publish-gittag.ps1 - Tweaks to variable names and git username & email.

--- tools/publish-gittag.ps1
+++ tools/publish-gittag.ps1
@@ -15,12 +15,12 @@ $ModuleManifest = Import-PowerShellDataFile -Path $ModuleManifestPath
 $Version = Get-Version -ModuleManifest $ModuleManifest
         
 # Create new tag
-$NewTagName = "v$Version"
-Write-Output "Creating tag: $NewTagName"
+$TagName = "v$Version"
+Write-Output "Creating tag: $TagName"
 
 # Commit changes
-git config --global user.name 'github-actions[bot]'
-git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+git config --global user.name 'github-ps-build'
+git config --global user.email 'github-ps-build@noreply.github.com'
 git add .
 git commit -m "Update version to $Version and update module manifest"
-git tag $NewTagName
+git tag $TagName
