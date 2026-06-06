0ae9a2cf1c5eddb4de8df1467f985b6bba718d87
Peter Laplain <peter@laplain.co.uk>
2026-06-06 14:22:27 +0100

    build.ps1 - replaced write-information with write-output

--- tools/build.ps1
+++ tools/build.ps1
@@ -2,24 +2,24 @@ param(
     [Parameter(Mandatory=$true)][string]$ModulePath
 )
 
-Write-Information "Starting build process for module at path: $ModulePath"
+Write-Output "Starting build process for module at path: $ModulePath"
 
 $Helpers = Get-ChildItem -Path tools\build\*.ps1 -Recurse
 
-Write-Information "Sourcing helper scripts from tools\build:"
+Write-Output "Sourcing helper scripts from tools\build:"
 foreach ($Helper in $Helpers) {
     . $Helper.FullName
 }
 
 # Get-ModuleManifest
-Write-Information "Retrieving module manifest for module at path: $ModulePath"
+Write-Output "Retrieving module manifest for module at path: $ModulePath"
 $ModuleManifestPath = Get-ModuleManifestPath -ModulePath $ModulePath
 $ModuleManifest = Import-PowerShellDataFile -Path $ModuleManifestPath
 
 # Get-Version
-Write-Information "Calculating new version based on module manifest and git tags"
+Write-Output "Calculating new version based on module manifest and git tags"
 $Version = Get-Version -ModuleManifest $ModuleManifest
 
 # Update-Manifest
-Write-Information "Updating module manifest for version: $Version"
+Write-Output "Updating module manifest for version: $Version"
 Update-Manifest -ModulePath $ModulePath -ModuleManifestPath $ModuleManifestPath -Version $Version
