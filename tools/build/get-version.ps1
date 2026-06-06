9034f67068c9f6989bf310e6179242f9b39eb39e
Peter Laplain <peter@laplain.co.uk>
2026-06-06 15:38:55 +0100

    get-version.ps1 - Call pwsh for git tags

--- tools/build/get-version.ps1
+++ tools/build/get-version.ps1
@@ -4,17 +4,8 @@ function Get-Version {
         [Parameter(Mandatory = $true)][hashtable]$ModuleManifest
     )
 
-    $CurrentLastExitCode = $LASTEXITCODE
-    $LatestTag = git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*" 2>&1
-    $LASTEXITCODE = $CurrentLastExitCode
-
-    if ($LatestTag.GetType().Name -eq "ErrorRecord" -and $LatestTag.Exception.Message -eq "fatal: No names found, cannot describe anything.") {
-        Write-Verbose "No tags found in git repository."
-        $LatestTag = $null   
-    }
-    elseif ($LatestTag.GetType().Name -eq "ErrorRecord") {
-        throw($LatestTag.Exception.Message)
-    }
+    # Running pwsh seperately to prevent LASTEXITCODE from being overwritten by git commands
+    $LatestTag = pwsh -Command 'git describe --tags --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*"'
 
     $ModuleVersion = $ModuleManifest.ModuleVersion
 
