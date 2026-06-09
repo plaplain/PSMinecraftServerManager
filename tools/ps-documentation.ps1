633812a024c53cb83a46d7e596bcbeb938c0c1fd
Peter Laplain <peter@laplain.co.uk>
2026-06-09 19:58:04 +0100

    Added ps-documentation.ps1

---
+++ tools/ps-documentation.ps1
@@ -0,0 +1,23 @@
+param(
+    [Parameter(Mandatory = $true)][string]$OutputPath,
+    [Parameter(Mandatory = $true)][string]$ModulePsdPath,
+    [Parameter(Mandatory = $true)][string]$ModuleName
+)
+
+Install-Module -Name Microsoft.PowerShell.PlatyPS -Force -ErrorAction Stop
+Import-Module Microsoft.PowerShell.PlatyPS -Force -ErrorAction Stop
+
+Import-Module -Path $ModulePath
+
+if(!(Test-Path -Path $OutputPath) -or !(Test-Path -Path $CodePath)) {
+    Write-Error "Output path '$OutputPath' or code path '$CodePath' does not exist."
+    exit 1
+}
+
+$NewMarkdownCommandHelpParams = @{
+    ModuleInfo = Get-Module -Name $ModuleName
+    OutputFolder = $OutputPath
+    WithModulePage = $true
+}
+
+New-MarkdownCommandHelp @NewMarkdownCommandHelpParams
