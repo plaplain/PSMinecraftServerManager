2efeea2f015d2cc9a0f97c4bd0d6c15e82a52689
Peter Laplain <peter@laplain.co.uk>
2026-06-09 20:20:40 +0100

    Start-MinecraftServer.ps1 - Added comments

--- src/public/Start-MinecraftServer.ps1
+++ src/public/Start-MinecraftServer.ps1
@@ -1,3 +1,13 @@
+<#
+ .SYNOPSIS
+     Starts a Minecraft server.
+ .DESCRIPTION
+     This function starts a Minecraft server in the specified directory.
+ .PARAMETER ServerDirectory
+     The directory where the Minecraft server is located.
+ .EXAMPLE
+     Start-MinecraftServer -ServerDirectory "C:\Minecraft\Server"
+ #>
 Function Start-MinecraftServer {
     [CmdletBinding(SupportsShouldProcess = $true)]
 
