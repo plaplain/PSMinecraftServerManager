fa685f9424bb9386994304ec80c5ef4f0855e7cc
Peter Laplain <peter@laplain.co.uk>
2026-06-12 18:40:35 +0100

    Trigger PSAnalyzer

--- tests/helpers/Test-FunctionThrowsWithMissingParameter.ps1
+++ tests/helpers/Test-FunctionThrowsWithMissingParameter.ps1
@@ -1,7 +1,7 @@
 
 function Test-FunctionThrowsWithMissingParameter {
     param(
-        #[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'FunctionName', Justification='False positive due to how Pester works.')]
+        #[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'FunctionName', Justification='False positive due to how Pester works.')] 
         [Parameter(Mandatory = $true)][string]$FunctionName
     )
 
