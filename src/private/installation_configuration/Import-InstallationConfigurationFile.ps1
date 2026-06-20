cc598aec59ae15f644e541ae9077a120e36c3bfc
Peter Laplain <peter@laplain.co.uk>
2026-06-20 19:42:29 +0100

    Added configuration class

--- src/private/installation_configuration/Import-InstallationConfigurationFile.ps1
+++
@@ -1,9 +0,0 @@
-function Import-InstallationConfigurationFile {
-    $ConfigurationFilePath = (Get-InstallationConfigurationPath).FullPath
-
-    if(!(Test-Paith -Path $ConfigurationFilePath)){
-        throw("Invalid configuration file path '$ConfigurationFilePath'")
-    }
-
-    Get-Content -Path $ConfigurationFilePath -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
-}
---
+++ src/private/installation_configuration/InstallationConfiguration.Class.ps1
@@ -0,0 +1,65 @@
+class InstallationConfiguration {
+    [hashtable]$Servers = @{}
+
+    # Constructor
+    InstallationConfiguration() {
+
+    }
+
+    InstallationConfiguration([string]$ConfigurationFilePath){
+        $this.LoadConfiguration($ConfigurationFilePath)
+    }
+
+    InitializeConfiguration() {
+        
+    }
+
+    [void]LoadConfiguration($Path) {
+        if(!(Test-Path $Path)){
+            throw('Invalid path')
+        }
+
+        $Configuration = Get-Content -Path $Path | ConvertFrom-Json
+
+        $this.Servers = $Configuration.Servers
+    }
+
+    [PSCustomObject]GetServer([string]$ServerName) {
+        return $this.Servers[$ServerName]
+    }
+
+    [PSCustomObject]GetAllServers() {
+        return $this.Servers
+    }
+
+    # Add server
+    [void]AddServer($ServerName, $InstallationFolder) {
+        if ($null -ne $this.Servers[$ServerName]) {
+            throw("A server by the name '$ServerName' already exists.")
+        }
+        
+        $ServerConfiguration = [PSCustomObject]@{
+            Name               = $ServerName
+            InstallationFolder = $InstallationFolder
+        }
+
+        $this.Servers.Add($ServerName, $ServerConfiguration)
+    }
+
+    # Remove Server
+    [void]RemoveServer($ServerName) {
+        $this.Servers.Remove($ServerName)
+    }
+
+    [void]SetServer($ServerName,$ServerObject) {
+        $this.Servers[$ServerName] = $ServerObject
+
+        if($ServerName -ne $ServerObject.Name){
+            $this.RemoveServer($ServerName)
+        }        
+    }
+
+    ExportConfigurationToFile($Path) {
+
+    }
+}
