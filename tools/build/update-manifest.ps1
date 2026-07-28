8d536c93ef8fcc0522866cd4cac62b3a92b2c3a0
Peter Laplain <peter@laplain.co.uk>
2026-07-28 20:37:35 +0100

    update-manifest - Corrected replace syntax typo

--- tools/build/update-manifest.ps1
+++ tools/build/update-manifest.ps1
@@ -22,18 +22,18 @@ function Update-Manifest {
     }
 
     # Get private functions
-    $PrivateFunctions = Get-ChildItem -Path "$ModulePath\Private" -Filter "*.ps1" -Recurse -ErrorAction Stop
+    $PrivateFunctions = Get-ChildItem -Path "$ModulePath\private" -Filter "*.ps1" -Recurse -ErrorAction Stop
     $NestedModules = @()
 
     foreach($Function in $PrivateFunctions){
-        $NestedModules += ".$($Function.FullName.Replace($ModulePath + '').Replace('\','/'))"
+        $NestedModules += ".$($Function.FullName.Replace($ModulePath,'').Replace('\','/'))"
     }
 
     # Get public cmdlets
-    $PublicCmdlets = Get-ChildItem -Path "$ModulePath\Public" -Filter "*.ps1" -Recurse -ErrorAction Stop
+    $PublicCmdlets = Get-ChildItem -Path "$ModulePath\public" -Filter "*.ps1" -Recurse -ErrorAction Stop
 
     foreach($Cmdlet in $PublicCmdlets){
-        $NestedModules += ".$($Cmdlet.FullName.Replace($ModulePath + '').Replace('\','/'))"
+        $NestedModules += ".$($Cmdlet.FullName.Replace($ModulePath,'').Replace('\','/'))"
     }
 
     Update-ModuleManifest -Path $ModuleManifestPath -NestedModules $NestedModules -CmdletsToExport $PublicCmdlets.BaseName -ModuleVersion $Version -ErrorAction Stop
