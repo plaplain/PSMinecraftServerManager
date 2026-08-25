e77e08fcaf2e5d5f9c7ee7ca158312d6f41e5e82
Peter Laplain <peter@laplain.co.uk>
2026-08-25 20:59:52 +0100

    MinecraftServerManager - Added Install-MinecraftServer integration tests

--- tests/MinecraftServerManager.Tests.ps1
+++ tests/MinecraftServerManager.Tests.ps1
@@ -65,7 +65,242 @@ Describe "Base Function & Class Tests" -Tag 'Unit' {
 }
 
 Describe "MinecraftServerManager Integration Tests" -Tag 'Integration' {
-    BeforeAll {}
+    BeforeAll {
+        if ($IsLinux) {
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'FolderPath', Justification = 'False positive due to how Pester works.')]
+            $FolderPath = '/home/minecraft'
+        }
+        else {
+            $FolderPath = 'C:\Temp\'
+        }
+
+        $MockVersionResponses = @{
+            '26.2-rc-2' = @(
+                [PSCustomObject]@{
+                    id        = 70
+                    time      = (Get-Date '06/01/2026')
+                    channel   = 'STABLE'
+                    commits   = @{
+                        sha     = '70eaed653d2260e42e510ea16e8fcb50152aa5ef'
+                        time    = (Get-Date '05/01/2026')
+                        message = 'Soft limit projectile list size (#13954)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.2/Stable'
+                        }
+                    }
+                },
+                [PSCustomObject]@{
+                    id        = 69
+                    time      = (Get-Date '04/01/2026')
+                    channel   = 'BETA'
+                    commits   = @{
+                        sha     = '76d2ac758cb3abe75aceefa88207443768f585c6'
+                        time    = (Get-Date '03/01/2026')
+                        message = 'Soft limit projectile list size (#13954)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.2/Beta'
+                        }
+                    }
+                },
+                [PSCustomObject]@{
+                    id        = 68
+                    time      = (Get-Date '02/01/2026')
+                    channel   = 'ALPHA'
+                    commits   = @{
+                        sha     = '3580fa4066c0081b96c4b3b2fb3a5ca2214a98c0'
+                        time    = (Get-Date '01/01/2026')
+                        message = 'Soft limit projectile list size (#13954)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.2/Alpha'
+                        }
+                    }
+                }
+            )
+            '26.1.2' = @(
+                [PSCustomObject]@{
+                    id = 60
+                    time = (Get-Date '11/12/2025')
+                    channel = 'STABLE'
+                    commits = @{
+                        sha = '70eaed653d2260e42e510ea15e8fcb50152aa5ef'
+                        time = (Get-Date '10/12/2025')
+                        message = 'Soft limit projectile list size (#13953)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.1/Stable'
+                        }
+                    }
+                },
+                [PSCustomObject]@{
+                    id        = 59
+                    time      = (Get-Date '09/12/2025')
+                    channel   = 'BETA'
+                    commits   = @{
+                        sha     = '76d2ac758cb3abe75aceeed88207443768f585c6'
+                        time    = (Get-Date '08/12/2025')
+                        message = 'Soft limit projectile list size (#13952)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.1/Beta'
+                        }
+                    }
+                },
+                [PSCustomObject]@{
+                    id        = 58
+                    time      = (Get-Date '07/12/2025')
+                    channel   = 'ALPHA'
+                    commits   = @{
+                        sha     = '3580fa4066c0081b96c4b5a2fb3a5ca2214a98c0'
+                        time    = (Get-Date '06/12/2025')
+                        message = 'Soft limit projectile list size (#13951)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.1/Alpha'
+                        }
+                    
+                    }
+                }
+            )
+            '26.1.1' = @(
+                [PSCustomObject]@{
+                    id = 57
+                    time = (Get-Date '05/12/2025')
+                    channel = 'STABLE'
+                    commits = @{
+                        sha = '70eaed653d2260e42e510ea15e8fcb50152aa5ef'
+                        time = (Get-Date '04/12/2025')
+                        message = 'Soft limit projectile list size (#13953)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.1/Stable'
+                        }
+                    }
+                },
+                [PSCustomObject]@{
+                    id        = 56
+                    time      = (Get-Date '03/12/2025')
+                    channel   = 'BETA'
+                    commits   = @{
+                        sha     = '76d2ac758cb3abe75aceeed88207443768f585c6'
+                        time    = (Get-Date '02/12/2025')
+                        message = 'Soft limit projectile list size (#13952)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.1/Beta'
+                        }
+                    }
+                },
+                [PSCustomObject]@{
+                    id        = 55
+                    time      = (Get-Date '01/12/2025')
+                    channel   = 'ALPHA'
+                    commits   = @{
+                        sha     = '3580fa4066c0081b96c4b5a2fb3a5ca2214a98c0'
+                        time    = (Get-Date '06/11/2025')
+                        message = 'Soft limit projectile list size (#13951)'
+                    }
+                    downloads = @{
+                        'server:default' = @{
+                            Url = 'https://fakeUrl.com/26.1/Alpha'
+                        }
+                    
+                    }
+                }
+            )
+        }
+
+        Mock -CommandName Invoke-RestMethod -ParameterFilter { $Uri -like "https://fill.papermc.io/v3/projects/paper/versions/26.2-rc-2/builds" } -MockWith {
+            $MockVersionResponses['26.2-rc-2']
+        }
+
+        Mock -CommandName Invoke-RestMethod -ParameterFilter { $Uri -like "https://fill.papermc.io/v3/projects/paper/versions/26.1.2/builds" } -MockWith {
+            $MockVersionResponses['26.1.2']
+        }
+
+        Mock -CommandName Invoke-RestMethod -ParameterFilter { $Uri -like "https://fill.papermc.io/v3/projects/paper/versions/26.1.1/builds" } -MockWith {
+            $MockVersionResponses['26.1.1']
+        }
+
+        Mock -CommandName Invoke-RestMethod -ParameterFilter { $Uri -eq 'https://fill.papermc.io/v3/projects/paper' } -MockWith {
+            @{
+                project  = [PSCustomObject]@{
+                    id   = 'paper'
+                    name = 'Paper'
+                }
+                versions = [PSCustomObject]@{
+                    '26.2' = @(
+                        '26.2-rc-2'
+                    )
+                    '26.1' = @(
+                        '26.1.2',
+                        '26.1.1'
+                    )
+                }
+            }
+        }
+
+        Mock -CommandName Invoke-RestMethod -ParameterFilter { $Uri -like 'https://launchermeta.mojang.com/mc/game/version_manifest_v2.json' } -MockWith {
+            [PSCustomObject]@{
+                latest = [PSCustomObject]@{
+                    release = '26.2'
+                    snapshot = '26.2'
+                }
+                versions = @(
+                    [PSCustomObject]@{
+                        id = '26.2'
+                        type = 'release'
+                        url = 'https://piston-meta.mojang.com/v1/packages/4c3cd3500ce8b9ea104c358a784634fedb2a610f/26.2.json'
+                        releaseTime = '16/06/2026 13:03:33'
+                    },
+                    [PSCustomObject]@{
+                        id = '26.1.2'
+                        type = 'release'
+                        url = 'https://piston-meta.mojang.com/v1/packages/e3510ae9ff09fba9410cbbb8a02bfd819632155d/26.1.2.json'
+                        releaseTime = '09/04/2026 11:12:23'
+                    },
+                    [PSCustomObject]@{
+                        id = '26.2-rc-2'
+                        type = 'snapshot'
+                        url = 'https://piston-meta.mojang.com/v1/packages/606a0e2eb54d953ac478774efcfcedcdf59f4cc5/26.2-rc-2.json'
+                        releaseTime = '12/06/2026 12:32:28'
+                    },
+                    [PSCustomObject]@{
+                        id = '26.2-rc-1'
+                        type = 'snapshot'
+                        url = 'https://piston-meta.mojang.com/v1/packages/f7053b943901bf8734b73723f43b31a9a8b6b776/26.2-rc-1.json'
+                        releaseTime = '11/06/2026 12:57:50'
+                    },
+                    [PSCustomObject]@{
+                        id = '26.2-pre-6'
+                        type = 'snapshot'
+                        url = 'https://piston-meta.mojang.com/v1/packages/020b103ce898979439db892c0c259d76936f559d/26.2-pre-6.json'
+                        releaseTime = '10/06/2026 13:20:24'
+                    }
+                )
+            }
+        }
+
+        Mock -CommandName Get-Command -ParameterFilter {$Name -like 'java'} -MockWith {}
+        Mock -CommandName New-Item -MockWith {}
+        Mock -CommandName Test-Path -ParameterFilter {$Path -like 'InstallationConfigurationFullPath'}
+        
+        # InstallationConfigurationClass Mocks
+        Mock -CommandName Test-Path -ParameterFilter {$Path -like $FolderPath} -MockWith {}
+        $ConfigurationFilePath = Join-Path -Path $FolderPath -ChildPath 'configuration.json'
+        Mock -CommandName Test-Path -ParameterFilter {$Path -like $ConfigurationFilePath} -MockWith {}
+        Mock -CommandName Out-File -MockWith {}
+    }
 
     Context "Module can load" {
         BeforeAll{
@@ -88,6 +323,12 @@ Describe "MinecraftServerManager Integration Tests" -Tag 'Integration' {
 
         It "Get-Module should contain cmdlets" -ForEach $Cmdlets {
             $GetModule.ExportedCommands.Keys | Should -Contain $Cmdlet
+        }
+    }
+
+    Context "Install-Minecraft" {
+        It "Should install vanilla"{
+            Install-MinecraftServer -ServerName 'IntegrationTest' -InstallationPath $FolderPath
         }
     }
 }
