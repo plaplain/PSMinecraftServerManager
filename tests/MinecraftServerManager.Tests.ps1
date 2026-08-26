7add2e03a9574a3c3425f0fd5ad64a6ca977721c
Peter Laplain <peter@laplain.co.uk>
2026-08-26 08:02:17 +0100

    Hardcoded C:\Temp\

--- tests/MinecraftServerManager.Tests.ps1
+++ tests/MinecraftServerManager.Tests.ps1
@@ -88,7 +88,7 @@ Describe "MinecraftServerManager Integration Tests" -Tag 'Integration' {
         Mock -ModuleName $ModuleName -CommandName Get-Command -ParameterFilter {$Name -like 'java'} -MockWith {}
 
         # New-FolderStructure Mocks
-        Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -eq $FolderPath} -MockWith {$true}
+        Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -eq 'C:\Temp\'} -MockWith {$true}
         Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -like $LiveFolderPath} -MockWith {$false}
         Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -like $BackupFolderPath} -MockWith {$false}
         Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -like $LogsFolderPath} -MockWith {$false}
