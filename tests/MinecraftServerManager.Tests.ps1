66fa4ae927feec82aa6c190b9a0b611aebf5e038
Peter Laplain <peter@laplain.co.uk>
2026-08-29 16:56:19 +0100

    Added configuration folder path test

--- tests/MinecraftServerManager.Tests.ps1
+++ tests/MinecraftServerManager.Tests.ps1
@@ -98,6 +98,7 @@ Describe "MinecraftServerManager Integration Tests" -Tag 'Integration' {
 
         # InstallationConfigurationClass Mocks
         Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -eq $InstallationFolderPath} -MockWith {$true}
+        Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -eq $ConfigurationFolderPath} -MockWith {$true}
         $ConfigurationFilePath = Join-Path -Path $ConfigurationFolderPath -ChildPath 'configuration.json'
         Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter {$Path -like $ConfigurationFilePath} -MockWith {$false}
         Mock -ModuleName $ModuleName -CommandName Out-File -MockWith {}
