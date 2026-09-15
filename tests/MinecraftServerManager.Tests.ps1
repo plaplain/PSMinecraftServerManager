5a873edda801a13eca72fa9a0056020788c1d2fb
Peter Laplain <peter@laplain.co.uk>
2026-09-15 19:50:32 +0100

    Reverted to mock

--- tests/MinecraftServerManager.Tests.ps1
+++ tests/MinecraftServerManager.Tests.ps1
@@ -453,16 +453,15 @@ Describe "MinecraftServerManager Integration Tests" -Tag 'Integration' {
         }
 
         It "Should start a minecraft server in interactive mode" {
-        $EulaScriptBlock = New-SequentialResultsMockBehavior -SequentialResults @(
-            $false,
-            $true,
-            $true,
-            $true,
-            $true
-        )
+            $EulaScriptBlock = New-SequentialResultsMockBehavior -SequentialResults @(
+                $false,
+                $true,
+                $true,
+                $true,
+                $true
+            )
 
-
-        #Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter { $Path -eq $EulaFilePath } -MockWith $EulaScriptBlock
+            Mock -ModuleName $ModuleName -CommandName Test-Path -ParameterFilter { $Path -eq $EulaFilePath } -MockWith $EulaScriptBlock
 
             $StartServerCall = Start-MinecraftServer -ServerName 'IntegrationTest' -InterativeMode
             { $StartServerCall } | Should -Not -Throw
